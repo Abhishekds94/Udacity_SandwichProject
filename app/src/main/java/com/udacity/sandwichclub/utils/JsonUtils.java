618db24767a90cda44a527b0082c7d18e93c9fc2
Abhishek D S <abhishekds94@gmail.com>
2019-11-04 19:07:09 -0600

    JSON Parsor file

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,15 @@
 package com.udacity.sandwichclub.utils;
 
-import com.udacity.sandwichclub.model.Sandwich;
-
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
-    }
+    private final static String TAG = JsonUtils.class.getSimpleName();
+
+    private final static String NAME_CODE = "name";
+    private final static String MAIN_NAME_CODE = "mainName";
+    private final static String ALSO_KNOWN_AS_CODE = "alsoKnownAs";
+    private final static String PLACE_OF_ORIGIN_CODE = "placeOfOrigin";
+    private final static String DESCRIPTION_CODE = "description";
+    private final static String IMAGE_CODE = "image";
+    private final static String INGREDIENTS_CODE = "ingredients";
+
 }
